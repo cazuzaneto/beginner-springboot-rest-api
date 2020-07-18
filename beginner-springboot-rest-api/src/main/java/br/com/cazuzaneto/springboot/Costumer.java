ec9bd5884dc0e131cf1fd4d4408a462139958b47
Cazuza Neto <cazuzant@gmail.com>
2020-07-18 17:56:58 -0300

    Rename package on beginner-springboot-rest-api project

--- beginner-springboot-rest-api/src/main/java/br/com/cazuzaneto/springboot/Costumer.java
+++ beginner-springboot-rest-api/src/main/java/br/com/cazuzaneto/springboot/Costumer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package br.com.cazuzaneto.springboot;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- beginner-springboot-rest-api/src/main/java/br/com/cazuzaneto/springboot/CostumerController.java
+++ beginner-springboot-rest-api/src/main/java/br/com/cazuzaneto/springboot/CostumerController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package br.com.cazuzaneto.springboot;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
